d45e0fb96e4d03708e311968b553465f1a2d2d21
Pratik Mehkarkar <pratikmehkarkar@gmail.com>
2022-01-03 14:19:09 -0800

    Count Primes

--- src/main/kotlin/CountPrime.kt
+++ src/main/kotlin/CountPrime.kt
@@ -1,2 +1,34 @@
-class CountPrime {
+import kotlin.math.ceil
+import kotlin.math.sqrt
+
+class CountPrime
+{
+    fun checkPrimeNumber(n: Int):Int
+    {
+        //No numbers less than 3 are prime
+        if (n<3) return 0
+
+        val limit = ceil(sqrt(n.toFloat())).toInt()
+
+        //Here we can understand which number is already checked
+        val loopCursor = BooleanArray(n) {false}
+        var count = 1
+
+        (3 until n step 2).forEach{ odd->
+            if (!loopCursor[odd])
+            {
+                if (odd < limit) (odd * odd until n step odd * 2).forEach{loopCursor[it] =true}
+                count++
+            }
+        }
+
+        return count
+    }
+}
+
+
+fun main(args: Array<String>)
+{
+    val x = CountPrime()
+    println(x.checkPrimeNumber(10))
 }
--- src/main/kotlin/LinkList.kt
+++ src/main/kotlin/LinkList.kt
@@ -1,23 +1,23 @@
-class Solution
-{
-    fun removeElements(head: ListNode?, `val`: Int): ListNode?
-    {
-        var cursor = head
-        val a = ListNode(0)
-        var headDummy = a
-        while(cursor!=null)
-        {
-            if (cursor.`val` != `val`)
-            {
-                headDummy.next = ListNode(cursor.`val`)
-                headDummy = headDummy.next
-            }
-            cursor = cursor.next
-        }
-        return a.next
-    }
-}
-fun main(args:Array<String>)
-{
-
-}
+//class Solution
+//{
+//    fun removeElements(head: ListNode?, `val`: Int): ListNode?
+//    {
+//        var cursor = head
+//        val a = ListNode(0)
+//        var headDummy = a
+//        while(cursor!=null)
+//        {
+//            if (cursor.`val` != `val`)
+//            {
+//                headDummy.next = ListNode(cursor.`val`)
+//                headDummy = headDummy.next
+//            }
+//            cursor = cursor.next
+//        }
+//        return a.next
+//    }
+//}
+//fun main(args:Array<String>)
+//{
+//
+//}
